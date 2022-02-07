20622cec61d3b856ec11eaa0e12e5225a66732b7
German <polik20003@gmail.com>
2022-02-07 22:14:16 +0300

    1

---
+++ lec.py
@@ -0,0 +1,34 @@
+a = 5
+b = 1.24
+i = None
+#print(a)
+#print(b)
+#print(type(a))
+i =654
+#print(type(b))
+#print(type(i))
+s='hello'
+#print(s)
+#print(type(s))
+#print ( a,"-",b ,"-", s )
+#print ("{}-{}-{}".format(a, b, s))
+#print (f"{a}-{b}-{s}")
+#print ("{1}-{2}-{0}".format(a, b, s))
+#j= True
+#print(type(j))
+#list = []
+#list2 = [1,4,7]
+#print(list2)
+#listOFstrings = ["one", "two", "three"]
+#print(listOFstrings)
+#v = input()
+#print(v)
+#d= int(input())
+n =1.3
+m = 2
+#z = round( n*m ,1)
+#z = round( n*m)
+m += 2
+print(m)
+
+
